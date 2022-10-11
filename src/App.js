0f8577c3c29e5f9050a8320a840337757f3fbf3e
adescode <adelaja444@gmail.com>
2022-10-11 21:46:35 +0100

    first init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+
+// Task 1: Avoid empty todo
+// Task 2: Clear input on add todo
+// Task 3: create `addTodo` function outside button onClick
 
 function App() {
+  const [todoList, setTodoList] = useState(['Somto']);
+  const [todoInput, setTodoInput] = useState('');
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <input onChange={(e) => setTodoInput(e.target.value)} />
+      <button
+        onClick={() => {
+          const tempTodoList = [...todoList];
+          tempTodoList.push(todoInput);
+          setTodoList(tempTodoList);
+        }}
+      >
+        ADD
+      </button>
+      <ul>
+        {todoList.map((value, index) => (
+          <li key={index}>{value}</li>
+        ))}
+      </ul>
     </div>
   );
 }
